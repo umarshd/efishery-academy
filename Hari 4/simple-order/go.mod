b7caf41597dd289aa382cd1de530be4e93fb1695
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-09-26 07:47:11 +0700

    Belajar Membuat CRUD tanpa DB

---
+++ Hari 4/simple-order/go.mod
@@ -0,0 +1,3 @@
+module simple-order
+
+go 1.17
---
+++ Hari 4/simple-order/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type Employee struct {
+	ID       int
+	Name     string
+	Age      int
+	Division string
+}
+
+var employees = []Employee{
+	{ID: 1, Name: "Budi Gunawan", Age: 32, Division: "Marketing"},
+	{ID: 2, Name: "Wawan Susanto", Age: 31, Division: "Management"},
+	{ID: 3, Name: "Jajan Nurjaman", Age: 38, Division: "Humas"},
+}
+
+var PORT = ":8080"
+
+func main() {
+	http.HandleFunc("/employees", getEmployees)
+	http.HandleFunc("/employee", createEmployee)
+
+	fmt.Println("Application is listening on port", PORT)
+	http.ListenAndServe(PORT, nil)
+}
+
+func getEmployees(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "GET" {
+		json.NewEncoder(w).Encode(employees)
+		return
+	}
+
+	http.Error(w, "Invalid method", http.StatusBadRequest)
+}
+
+func createEmployee(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		age := r.FormValue("age")
+		division := r.FormValue("division")
+
+		convertAge, err := strconv.Atoi(age)
+
+		if err != nil {
+			http.Error(w, "Invalid age", http.StatusBadRequest)
+			return
+		}
+
+		newEmployee := Employee{
+			ID:       len(employees) + 1,
+			Name:     name,
+			Age:      convertAge,
+			Division: division,
+		}
+
+		employees = append(employees, newEmployee)
+
+		fmt.Fprintf(w, name+" berhasil didaftarkan")
+		return
+	}
+
+	http.Error(w, "Invalid method", http.StatusBadRequest)
+
+}
